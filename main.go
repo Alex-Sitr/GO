31e3dbba5a7bf4a49b907e6287ca44465c16e1df
Alex-Sitr <siter.pro@gmail.com>
2025-06-29 16:57:30 +0300

    Update main.go
    калькулятор индекса массы тела

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	const IMTPower = 2
+	var userHeight float64
+	var userKg float64
+	fmt.Print("__ Калькулятор индекса массы тела ___\n")
+	fmt.Print("Введите ссввой рост: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите ссввой вес: ")
+	fmt.Scan(&userKg)
+	IMT := userKg / math.Pow(userHeight, IMTPower)
+	fmt.Print("Ваш индекс массы тела: ")
+	fmt.Print(IMT, "\n")
 }
