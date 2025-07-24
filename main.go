28799e418d95fdfadc994a3d6ff8140e3521c6cf
Alex-Sitr <siter.pro@gmail.com>
2025-07-24 14:59:16 +0300

    read cmd and out to display

--- main.go
+++ main.go
@@ -1,18 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	user := NewUser(
-		"Вася",
-		500,
-		"79118707090",
-		false,
-		6.0,
-	)
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
 
-	fmt.Println("User:", user)
-	// user.RatingUP(2.5)
-	// fmt.Println("Рейтинг после:", user.Rating)
+		fmt.Print("Ввидите команду: ")
 
+		if ok := scanner.Scan(); !ok {
+			fmt.Println("Ошибка ввода!")
+			return
+		}
+
+		text := scanner.Text()
+
+		fields := strings.Fields(text)
+
+		if len(fields) == 0 {
+			fmt.Println("Вы ничего не ввели!")
+			return
+		}
+
+		cmd := fields[0]
+
+		if cmd == "выйти" {
+			fmt.Println("До скорого!")
+			return
+		}
+
+		if cmd == "добавить" {
+			str := ""
+			for i := 1; i < len(fields); i++ {
+				str += fields[i]
+				if i != len(fields)-1 {
+					str += " "
+				}
+			}
+			fmt.Println("Вы хотите добавить: ", str)
+		} else if cmd == "удалить" {
+			str := ""
+			for i := 1; i < len(fields); i++ {
+				str += fields[i]
+				if i != len(fields)-1 {
+					str += " "
+				}
+			}
+			fmt.Println("Вы ажется хотите удалить: ", str)
+		} else if cmd == "help" {
+			fmt.Println("Команда: help")
+			fmt.Println("-- эта команда выводит список доступных команд")
+			fmt.Println("")
+			fmt.Println("Команда: добавить {что нужно добавить}")
+			fmt.Println("-- эта команда позволяет добавлять что-то")
+			fmt.Println("")
+			fmt.Println("Команда: удалить {что нужно удалить}")
+			fmt.Println("-- эта команда позволяет удалять что-то")
+			fmt.Println("")
+		} else {
+			fmt.Println("вы вели неизвестную команду:")
+		}
+	}
 }
